ed374ea29afd03209543aadb0fb20c367f94fe81
Pongsathorn Choragam <oleefplus@gmail.com>
2020-01-13 15:00:32 +0700

    del

--- components/container.js
+++
@@ -1,4 +0,0 @@
-const Container = ({ children }) => {
-  return <div className="content-wrapper">{children}</div>;
-};
-export default Container;
--- server.js
+++
@@ -1,7 +0,0 @@
-// require("dotenv").config();
-// const dev = process.env.NODE_ENV !== "production";
-// const next = require("next");
-// const app = next({ dev });
-// app.prepare().then(() => {
-//   app.setAssetPrefix(process.env.APP_DOMAIN);
-// });
